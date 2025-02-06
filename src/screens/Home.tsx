596c0cddb2f02afd810b32a53c3fd64fd31fd7bb
Miguel Tibincoski <tibincoskimiguel@gmail.com>
2025-02-06 17:24:51 -0300

    adds PostCSS configuration, class utilities, and initial Tailwind CSS setup, as well as shadcn ui configuration

---
+++ src/screens/Home.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Home() {
+  return (
+    <>
+      <Card className="min-w-[350px] max-w-[400px]">
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>Hi!</CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+}
